c7d4d346c49e9342f3a44401c20026a167bbc480
AJ3907 <es12b1004@iith.ac.in>
2014-03-26 12:27:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,9 +4,13 @@ $flag=0;
 $s="";
 if($_POST)
 {
-   $ph=$_POST['ph'];
-   $pass=$_POST['pass'];
-   file_put_contents('a.txt', $pass);
+   $phone=$_POST['Phone'];
+   $pass=$_POST['Password'];
+   $bloodgroup=$_POST['BloodGroup'];
+   $name=$_POST['Name'];
+   $city=$_POST['City'];
+
+   //file_put_contents('a.txt', $pass);
    
 }
 
@@ -22,15 +26,27 @@ if($_POST)
    $sql =<<<EOF
       SELECT * from PROFILE;
 EOF;
-
+$n = 0;
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-      if(strcmp($ph,$row['PHONE'])==0 && strcmp($pass,$row['PASS'])==0)
-         $flag=1;
+      if(strcmp($ph,$row['PHONE'])==0)
+      {
+         $flag=1;     
+         echo "0"; //User already exists.
+      }
+n=$row['ID'];
+         
    }
-   if($flag==1)
-      echo "1";
-   else
-      echo "0";
+
+   if($flag==0)
+   {
+      $sql =<<<EOF
+      INSERT INTO PROFILE (ID,PHONE,NAME,PASSWORD,BLOODGROUP,CITY);
+      VALUES (n, $phone, $name, $password, $bloodgroup, $city);
+EOF;
+echo "1";// User registered
+   }
+   $db->exec($sql);
+   
    $db->close();
 ?>
