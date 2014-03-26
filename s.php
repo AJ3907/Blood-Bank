39ddc672218364753b1bbe8429edcb8e151f4dff
AJ3907 <es12b1004@iith.ac.in>
2014-03-26 11:51:19 +0530

    Create s.php

---
+++ s.php
@@ -0,0 +1,32 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      CREATE TABLE PROFILE
+      (ID INT PRIMARY KEY     NOT NULL,
+      PHONE           TEXT    NOT NULL,
+      PASS            TEXT     NOT NULL);
+EOF;
+
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Table created successfully\n";
+   }
+   
+   $db->close();
+
+?>
